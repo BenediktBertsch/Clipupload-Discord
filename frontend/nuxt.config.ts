6a43ceccc9e1559409af6b972a41ebf52ed3af24
Benedikt Bertsch <benedikt.bertsch@gmail.com>
2024-06-25 23:00:54 +0200

    Fix: frontend build

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,13 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 const development = process.env.NODE_ENV;
 export default defineNuxtConfig({
+  hooks: {
+    close: () => {
+      // @see https://github.com/nuxt/cli/issues/169#issuecomment-1729300497
+      // Workaround for https://github.com/nuxt/cli/issues/169
+      process.exit(0)
+    }
+  },
   devtools: {
     enabled: true,
 
